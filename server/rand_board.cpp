42b0fe51398b9931b0be839946e46a5d7f091684
Iwabuchi Takamasa <16403.nit@gmail.com>
2018-05-10 17:47:41 +0900

    盤面のデータ構造を構造体配列に変更

--- server/rand_board.cpp
+++ server/rand_board.cpp
@@ -1,6 +1,6 @@
 #include "rand_board.h"
 
-int init::rand_board(int*board, int row, int column) {
+int init::rand_board(struct board *board, int row, int column) {
 	std::random_device rnd;     // 非決定的な乱数生成器を生成
 	std::mt19937 mt(rnd());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
 	std::uniform_int_distribution<> rand_score(-16, 33);     // [-16, 32] 範囲の一様乱数
@@ -11,7 +11,7 @@ int init::rand_board(int*board, int row, int column) {
 			if (tmp > 0) {     //正の乱数の割合を高める
 				tmp /= 2;
 			}
-			board[i * column + j] = tmp;
+			board[i * column + j].point = tmp;
 		}
 	}
 
--- server/rand_board.h
+++ server/rand_board.h
@@ -2,8 +2,14 @@
 
 #include <random>
 
+struct board
+{
+	int point;	//マスの点数
+	int state;	//マスの状態 0:空 1:自陣 2:敵陣
+};
+
 class init {
 
 public:
-	int rand_board(int *board, int row, int column); //*board:2次元配列 row:行 column:列
+	int rand_board(struct board *board, int row, int column); //*board:2次元配列 row:行 column:列
 };
