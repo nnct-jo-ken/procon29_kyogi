36c22b58a06bd7b4130b6999afff86cb3fc105dc
Iwabuchi Takamasa <16403.nit@gmail.com>
2018-05-24 14:32:07 +0900

    乱数でフィールドを埋める & mainファイル作成

--- GUI/rand_board.cpp
+++ GUI/rand_board.cpp
@@ -3,15 +3,48 @@
 int init::rand_board(struct board *board, int row, int column) {
 	std::random_device rnd;     // 非決定的な乱数生成器を生成
 	std::mt19937 mt(rnd());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
-	std::uniform_int_distribution<> rand_score(-16, 33);     // [-16, 32] 範囲の一様乱数
+	std::uniform_int_distribution<> rand_score(-16, 16*9);     // [-16, 16*9] 範囲の一様乱数 マイナスの点を10%近くにする
+
+	std::random_device state;     // 非決定的な乱数生成器を生成
+	std::mt19937 mt_state(state());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
+	std::uniform_int_distribution<> rand_state(0, 2);     // 0:水平・垂直 1:水平 2:垂直
 
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < column; j++) {
 			int tmp = rand_score(mt);
 			if (tmp > 0) {     //正の乱数の割合を高める
-				tmp /= 2;
+				tmp /= 9;
 			}
-			board[i * column + j].point = tmp;
+			int status = rand_state(mt_state);
+
+			board[i * column + j].point = tmp;	//とにかく乱数を生成
+
+			if (status == 0 || status == 1) {	//水平に線対称
+				if (row % 2 == 0) {	//偶数
+					if (i > row / 2) {
+						board[i * column + j] = board[(row - i) * column + j];
+					}
+				}
+				else {	//奇数
+					if (i > row / 2 + 1) {
+						board[i * column + j] = board[(row - i + 1) * column + j];
+					}
+				}
+			}
+
+			if (status == 0 | status == 2) {	//垂直に線対称
+				if (column % 2 == 0) {	//偶数
+					if (j > column / 2) {
+						board[i * column + j] = board[i * column + (column - j)];
+					}
+				}
+				else {	//奇数
+					if (j > column / 2 + 1) {
+						board[i * column + j] = board[i * column + (column - j + 1)];
+					}
+				}
+			}
+
 		}
 	}
 
