22f35c6139d5473b0d20fab89b262043aed349a2
Iwabuchi Takamasa <16403.nit@gmail.com>
2018-05-24 18:02:27 +0900

    フィールドを乱数で埋める（たぶん完成）& 確認用の表示機能

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -3,13 +3,13 @@
 #include "main.h"
 
 int main(void) {
-	struct board field[10][10];
+	struct board field[11][11];
 
 	init func_field;
-	func_field.rand_board(*field, 10, 10);
+	func_field.rand_board(*field, 11, 11);
 
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
+	for (int i = 0; i < 11; i++) {
+		for (int j = 0; j < 11; j++) {
 			printf("%4d ", field[i][j].point);
 		}
 		putchar('\n');
--- GUI/rand_board.cpp
+++ GUI/rand_board.cpp
@@ -9,38 +9,40 @@ int init::rand_board(struct board *board, int row, int column) {
 	std::mt19937 mt_state(state());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
 	std::uniform_int_distribution<> rand_state(0, 2);     // 0:水平・垂直 1:水平 2:垂直
 
+	int status = rand_state(mt_state);	//線対称の位置を乱数で決定
+	status = 0;
+
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < column; j++) {
 			int tmp = rand_score(mt);
 			if (tmp > 0) {     //正の乱数の割合を高める
 				tmp /= 9;
 			}
-			int status = rand_state(mt_state);
 
 			board[i * column + j].point = tmp;	//とにかく乱数を生成
 
 			if (status == 0 || status == 1) {	//水平に線対称
 				if (row % 2 == 0) {	//偶数
-					if (i > row / 2) {
-						board[i * column + j] = board[(row - i) * column + j];
+					if (i > row / 2 - 1) {
+						board[i * column + j].point = board[(row - i - 1) * column + j].point;
 					}
 				}
 				else {	//奇数
-					if (i > row / 2 + 1) {
-						board[i * column + j] = board[(row - i + 1) * column + j];
+					if (i > row / 2) {
+						board[i * column + j].point = board[(row - i - 1) * column + j].point;
 					}
 				}
 			}
 
 			if (status == 0 || status == 2) {	//垂直に線対称
 				if (column % 2 == 0) {	//偶数
-					if (j > column / 2) {
-						board[i * column + j] = board[i * column + (column - j)];
+					if (j > column / 2 - 1) {
+						board[i * column + j].point = board[i * column + (column - j - 1)].point;
 					}
 				}
 				else {	//奇数
-					if (j > column / 2 + 1) {
-						board[i * column + j] = board[i * column + (column - j + 1)];
+					if (j > column / 2) {
+						board[i * column + j].point = board[i * column + (column - j - 1)].point;
 					}
 				}
 			}
